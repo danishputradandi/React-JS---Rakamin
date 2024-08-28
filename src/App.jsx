6653bf9779abce50fa947f232323479cfa2d4051
Depede <masdanishpd@gmail.com>
2024-08-28 14:18:47 +0700

    Menubar tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar";
+import Menubar from "./components/Menubar";
 
 function App() {
   return (
     <div className="bg-blue-100 min-h-screen">
       <Navbar />
+      <Menubar />
     </div>
   );
 }
